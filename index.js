c4ff0fa8aac84c0f0afa47a2d2da4099a5e9eda8
Whitewolf185 <matvey2001xxl1976@mail.ru>
2021-07-07 17:29:22 +0300

    why "Done" shows too early????

--- index.js
+++ index.js
@@ -82,195 +82,132 @@ function cleanOut () {
     })},1000)
 }
 
-let check_dir = async function (path) {
-    let result;
-    await fs.stat(path, (err) => {
-       if(!err){
-           result = 'Directory is existing';
-       }
-
-       else if (err.code === 'ENOENT'){
-           fs.mkdir(path, (e) => {
-               if(e){
-                   result = e;
-               }
-           });
-       }
-    });
-    return result;
-}
-
-let dwld_ch = async function(source, ch, img_count){
+let dwld_ch = function(source, ch, img_count){
     return async function (callback){
         await fs.mkdir(__dirname + '/chapter'+ch+'/', (e) => {
-            if(e.code === 'EEXIST'){
-                console.log('Directory has already exist');
-            }
-            else if (e) {
-                throw e;
+            if(e){
+                if(e.code === 'EEXIST'){
+                    console.log('Directory has already exist');
+                }
+                else {
+                    throw e;
+                }
             }
         })
 
 
-        let filesPath = [];
         let b = [];
         let path = "";
 
 
         for (let i = 1; i < img_count + 1; i++) {
             if (i < 10){
-                path = "00"+i+".png";
+                path = source.path+"00"+i+".png";
             }
             else{
-                path = "0"+i+".png"
+                path = source.path+"0"+i+".png";
             }
 
             let a = (optionPath) => {
                 return async function (callback) {
-                    source.path += optionPath;
-                    const res = await fetch(source.path, source.options);
+                    let local_path = optionPath;
+                    const res = await fetch(local_path, source.options);
                     if(res.ok) {
                         const fileStream = fs.createWriteStream(__dirname + __dirname + '/chapter' + ch
                             + '/img' + i + '.png');
+                        await new Promise((resolve, reject) => {
+                            res.body.pipe(fileStream)
+                            res.body.on("error", reject);
+                            fileStream.on("finish", resolve);
+                        })
 
                         callback(null, i);
                     }
                 }
             }
-            filesPath.push({
-                id: i,
-                path: __dirname + "/chapter1/img" + i
-            });//TODO надо расставить в нужных местах нормальные имена файлов
 
 
             b.push(a(path));
 
         }
+
+        await parallel(b, (err, result) => {
+            console.log("I've completed tasks ", result);
+            if(err) throw err;
+        })
+
+        callback(null, "chapter " + ch);
 
     }
 }
 
 let ch_chooser = async function (chapters_count){
     await fs.mkdir(__dirname + '/chapters/', (e) => {
-        if(e.code === 'EEXIST'){
-            console.log('Directory has already exist');
-        }
-        else if (e) {
-            throw e;
+        if(e){
+            if(e.code === 'EEXIST'){
+                console.log('Directory has already exist');
+            }
+            else {
+                throw e;
+            }
         }
     })
 
-    let tasks_pic = [];
+    new Promise(resolve => {
+        let tasks_pic = [];
 
-    for (let i = 1; i <= chapters_count; i++) {
-        let source = sources[0];
-        source.path += i + '/';
-        tasks_pic.push(dwld_ch(source,i,ch_list[i-1]));
-    }
-
-    parallel(tasks_pic, (err, result) => {
-        console.log("ch_chooser ", result);
-    })
-}
-
-let dwld_imgs = new Promise(((resolve, reject) => {
-    check_dir(__dirname + '/chapter1/').then(a => {
-        let filesPath = [];
-        let b = [];
-        let path = "";
-        for (let i = 1; i < 52; i++) {
-            if (i < 10){
-                path = URL+"00"+i+".png";
-            }
-            else{
-                path = URL+"0"+i+".png"
-            }
-
-            let a = (optionPath) => {
-                return function (callback){
-                    options.path = optionPath;
-                    let req = https.request(options, function (res) {
-                        let chunks = [];
-
-                        if(res.statusCode > 300){
-                            return;
-                        }
-
-                        res.on("data", function (chunk) {
-                            chunks.push(chunk);
-                        });
-
-                        res.on("end", function (chunk) {
-                            let body = Buffer.concat(chunks);
-                            writeFile(__dirname + "/chapter1/img_" + i + ".png", body)
-                                .then(result => {
-                                    console.log(optionPath + " ready")
-                                    callback(null,i);
-                                })
-                        });
-
-                        res.on("error", function (error) {
-                            reject(error);
-                        });
-                    });
-                    req.end();
-                }
-            }
-            filesPath.push({
-                id: i,
-                path: __dirname + "/chapter1/img" + i
-            });
-
-
-            b.push(a(path));
-
+        for (let i = 1; i <= chapters_count; i++) {
+            let source = sources[0];
+            source.path += i + '/';
+            tasks_pic.push(dwld_ch(source,i,ch_list[i-1]));
         }
-        //console.log(filesPath);
-        parallel(b,(err,result) => {
-            console.log(result);
-            if (err) throw err;
-            setTimeout(() =>{
-                resolve(filesPath)
-            }, 1000);
+
+        parallel(tasks_pic, (err, result) => {
+            console.log("ch_chooser ", result);
+
+            resolve();
         })
     })
+        .then(
+            () => {
+                console.log("im generating pdf file...");
 
-}));
+                exec('./img2pdf.sh chapter1 chapter_1.pdf')
+                    .then(
+                        output => {
+                            console.log(output.stdout);
+                            /*send_to_Email(__dirname + "/chapter_1.pdf", "Abyss_ch1.pdf")
+                                .then(result =>{
+                                    // cleanOut();
+                                })*/
+                        }
+                    )
+                    .catch(
+                        stderr => {
+                            console.log(stderr);
+                        }
+                    )
 
-dwld_imgs
-    .then(
-        filesPath => {
-            filesPath.sort((lhs, rhs) => {
-                return lhs.id - rhs.id;
-            });
-            console.log("im generating pdf file...");
+            }
+        )
 
-            exec('./img2pdf.sh chapter1 chapter_1.pdf')
-                .then(
-                    output => {
-                        console.log(output.stdout);
-                        /*send_to_Email(__dirname + "/chapter_1.pdf", "Abyss_ch1.pdf")
-                            .then(result =>{
-                                // cleanOut();
-                            })*/
-                    }
-                )
-                .catch(
-                    stderr => {
-                        console.log(stderr);
-                    }
-                )
+        .catch(
+            err => {
+                console.log(err);
 
-        }
-    )
+                //clean out all files, when error
+                // cleanOut();
+            }
+        )
 
-    .catch(
-        err => {
-            console.log(err);
 
-            //clean out all files, when error
-            cleanOut();
-        }
-    )
+}
+
+
+
+ch_chooser(16).then(() => {
+    console.log("Done");
+})
+
 
 
