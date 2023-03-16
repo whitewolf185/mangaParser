4308d7e4dfd24625aa2d6efa8805da1243e0f9c6
Matvey Volkov <whitewolf.mot185@gmail.com>
2023-03-17 00:31:47 +0300

    first migrations was created

---
+++ migrations/20230316235732_init_db.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL qufsdery';
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+-- +goose StatementEnd
